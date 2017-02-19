7d560b8de6ac8c2519bf8231497de485801cac1d
Christopher Lopez <chlo1259@gmail.com>
2017-02-19 12:35:01 +0000

    upgrade done

--- app/authenticators/cleaning-game.js
+++ app/authenticators/cleaning-game.js
@@ -5,38 +5,65 @@ import Ember from 'ember';
 const USER_ID_KEY = 'cg_user_id_key';
 
 const {
-  RSVP: { resolve },
+  RSVP: { reject, resolve },
   inject: { service }
 } = Ember;
 
 export default Base.extend({
   store: service(),
+  session: service(),
 
   restore() {
-
-  },
-  authenticate(name) {
-    let id = localStorage.getItem(USER_ID_KEY);
-
-    if (id === null) {
-      return this.createUser(name);
+    if (!this.hasSavedUser()) {
+      return reject();
     }
 
-    let store = this.get('store');
-    return store.findRecord('user', id).then((user) => {
+    return this.getCurrentUser().then((user) => {
+      this.setCurrentUser(user);
+      resolve(user);
+    });
+  },
+
+  authenticate(name) {
+    if (!this.hasSavedUser()) {
+      return this.createUser(name).then((user) => {
+        this.setCurrentUser(user);
+        resolve(user);
+      });
+    }
+
+    this.getCurrentUser().then((user) => {
+      localStorage.setItem(USER_ID_KEY, user.get('id'));
       return resolve(user);
     })
     .catch(() => {
-      return this.createUser(name);
+      return this.createUser(name).then((user) => {
+        this.setCurrentUser(user);
+        resolve(user);
+      });
     });
   },
 
   invalidate() {
-
+    // @todo remove user?
+    localStorage.removeItem(USER_ID_KEY);
   },
 
   createUser(name) {
     return this.get('store').createRecord('user', { name }).save();
-  }
+  },
 
+  getCurrentUser() {
+    let id = localStorage.getItem(USER_ID_KEY);
+    return this.get('store').findRecord('user', id);
+  },
+
+  hasSavedUser() {
+    return localStorage.getItem(USER_ID_KEY) !== null;
+  },
+
+  setCurrentUser(user) {
+    this.set('session.currentUser', user);
+    localStorage.setItem(USER_ID_KEY, user.get('id'));
+  }
 });
