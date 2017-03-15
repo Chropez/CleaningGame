768191738b0951aa4d57ee7fa538b50d613af79f
Christopher Lopez <chlo1259@gmail.com>
2017-03-15 21:32:50 +0100

    share url

--- app/components/cg-game-card.js
+++ app/components/cg-game-card.js
@@ -6,7 +6,8 @@ const {
   computed: {
     gt,
     mapBy,
-    filter
+    filter,
+    or
   },
   inject: { service }
 } = Ember;
@@ -17,18 +18,23 @@ export default Component.extend({
 
   playerUsers: mapBy('game.players', 'user'),
   hasOtherPlayers: gt('game.players.length', 1),
+
   otherUsers: filter('playerUsers.@each.id', function(user) {
     let currentUserId = this.get('session.currentUser.id');
     return user.get('id') !== currentUserId;
   }),
+
   otherUsersNames: mapBy('otherUsers', 'name'),
+
   playersText: computed('playerUsers.@each.id', function() {
     let hasOtherPlayers = this.get('hasOtherPlayers');
     if (!hasOtherPlayers) {
       return `Bara du i spelet`;
     }
+
     let userNames = this.get('otherUsersNames');
     let text = '';
+
     userNames.forEach((userName, index) => {
       if (index === userNames.length - 1 && index !== 0) {
         text += ' och '
@@ -38,10 +44,31 @@ export default Component.extend({
       text += userName;
     })
     return `Med ${text}`;
+  }),
 
+  isIPhone: computed(function() {
+    return !!navigator.userAgent.match(/(iPad|iPhone|iPod)/g);
+  }),
+
+  isAndroid: computed(function() {
+    return !!navigator.userAgent.toLowerCase().match(/(android)/g);
+  }),
+
+  isPhone: or('isIPhone', 'isAndroid'),
+
+  smsLinkUrl: computed('game.id', function() {
+    let separator = this.get('isAndroid') ? '?' : '&';
+    let url = this.get('linkUrl');
+    return `sms:${separator}body=Ska vi spela städspelet? Joina här ${url}`;
+  }),
+
+  linkUrl: computed('game.id', function() {
+    return `${window.location.href}${this.get('game.id')}`;
   }),
 
   actions: {
-    // click
+    shareGame() {
+      this.toggleProperty('showShareDialog');
+    }
   }
 });
